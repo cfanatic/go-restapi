8a6b471daf0a666b42fba6c0f0bf902a0df7300f
Arnd <webmaster@codefanatic.de>
2020-04-28 22:46:19 +0200

    Update build script for different MySQL deployment modes

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,11 @@
 #
 # This script builds the application from source using Docker
 
+# Build image
 docker build -t netchat:latest -f ../Dockerfile ../
-docker run --name netchat -d -p 1025:1025 --link mysql:db netchat:latest
+
+# Run container in combination with a MySQL container
+# docker run --name netchat -d -p 1025:1025 --link mysql:db netchat:latest
+
+# Run container in combination with a local instance of MySQL
+docker run --name netchat -d -p 1025:1025 netchat:latest
