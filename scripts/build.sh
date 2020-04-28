81cd3c21571ad20197dc434e1e913f93d89b7d81
Arnd <webmaster@codefanatic.de>
2020-04-28 23:09:16 +0200

    Update Docker build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,16 @@
 #
 # This script builds the application from source using Docker
 
+# Clean setup
+docker stop netchat
+docker rm netchat
+docker rmi netchat
+
 # Build image
 docker build -t netchat:latest -f ../Dockerfile ../
 
 # Run container in combination with a MySQL container
-# docker run --name netchat -d -p 1025:1025 --link mysql:db netchat:latest
+# docker run --name netchat -d --network host --link mysql:db netchat:latest
 
 # Run container in combination with a local instance of MySQL
-docker run --name netchat -d -p 1025:1025 netchat:latest
+docker run --name netchat -d --network host netchat:latest
